e9e017d3eb2f70ccca24a197b7382cd92f4683c8
Edwin L <cslasher@gmail.com>
2017-02-01 02:55:08 +0800

    Add 4th solution

---
+++ 4.goodOldForm.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var app = express()
+var bodyparser = require('body-parser')
+
+app.use(bodyparser.urlencoded({extended: false}))
+
+app.post('/form', function(req, res) {
+    res.end(req.body.str.split('').reverse().join(''))
+})
+app.listen(process.argv[2])
